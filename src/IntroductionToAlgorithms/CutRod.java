a035b4683dcbb527f34245463e0149d54b7075c5
Vic <985695123@qq.com>
2018-11-12 20:56:00 +0800

    自顶向下的CutRod过程，加入了备忘机制 P207-208

--- src/IntroductionToAlgorithms/CutRod.java
+++ src/IntroductionToAlgorithms/CutRod.java
@@ -14,4 +14,27 @@ public class CutRod {
         }
         return a ;
     }
+
+    // 自顶向下的CutRod过程，加入了备忘机制
+    public static int memoizedCudRod(int[] prices, int n) {
+        int[] m = new int[n];
+        for (int i= 1; i <= n; i ++ ) {
+            m[i] = 0;
+        }
+        return 0;
+    }
+
+    public static int memoizedCutRodAux(int[] prices, int n, int[] m) {
+        if (m[n] > 0) {
+            return m[n];
+        }
+        int q = 0;
+        {
+            for (int i= 1; i <= n; i ++ ){
+                q = Integer.max(q, prices[i] + memoizedCutRodAux(prices, n - i, m));
+            }
+        }
+        m[n] = q;
+        return q;
+    }
 }
